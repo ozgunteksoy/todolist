3f21f0edfc688fa36a1698f484a5240d77413046
crazyhaller <suvigyamadrid@gmail.com>
2022-11-11 20:41:26 +0530

    Passing data from site to server

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
 const express = require("express");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+
+var items = ["Get Productive", "Drink Water", "Eat Food"];
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get("/", function(req, res){
+
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;    
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;    
-        default:
-            console.log("Error! Current day is equal to: "+ currentDay);
-    }
+    var day = today.toLocaleDateString("en-US", options)
+    
+    res.render("list", {kindOfDay: day, newListItems: items});
+})
 
-    res.render("list", {kindOfDay: day});
+app.post("/", function(req, res){
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
 })
 
 app.listen(3000, function(){
