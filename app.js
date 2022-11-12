08c3a02fa45160b80d68391e84cf1b82a218dca3
crazyhaller <suvigyamadrid@gmail.com>
2022-11-12 15:07:24 +0530

    Add css and styles

--- app.js
+++ app.js
@@ -8,28 +8,33 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 
 app.get("/", function(req, res){
 
-    var today = new Date();
-    var options = {
+    let today = new Date();
+
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options)
+    let day = today.toLocaleDateString("en-US", options)
     
     res.render("list", {kindOfDay: day, newListItems: items});
 })
 
+
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
 
     items.push(item);
 
     res.redirect("/");
 })
+
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
